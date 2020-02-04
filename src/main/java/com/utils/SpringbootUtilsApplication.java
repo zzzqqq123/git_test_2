b32a49befb0243d6c6f3c8b5426b0c1e9f1ef755
zzzqqq123 <qinglian@163.com>
2020-02-04 13:35:39 +0800

    add1

--- src/main/java/com/utils/SpringbootUtilsApplication.java
+++ src/main/java/com/utils/SpringbootUtilsApplication.java
@@ -20,10 +20,8 @@ public class SpringbootUtilsApplication {
     public void test() throws Exception {
         throw  new MyException("fa");
         //test
-<<<<<<< HEAD
         //test2
-=======
->>>>>>> 0d1fa24a32c231f80839daac378b9d035eed5b5b
+
     }
 
 
